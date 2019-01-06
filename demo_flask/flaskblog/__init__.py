41fbca25ff806fe65f64267da5003aa89ff3d8cb
Abhishek Kumar Singh <kumar.abh494@gmail.com>
2019-01-07 00:35:46 +0530

    Initial Comit

---
+++ demo_flask/flaskblog/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '3e69d8d66f3daeaa229143b9d62055ee'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+
+from flaskblog import routes
